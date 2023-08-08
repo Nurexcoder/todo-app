91d6e7d8bb69698a44306cc6e03e3a8550dc9cac
Hibjul Ahmed <hibjulahmed@gmail.com>
2023-08-08 19:15:13 +0530

    fixed search bg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
         </div>
         <div className="flex items-center gap-x-5">
           <div className='flex items-center rounded-full border border-blue-400 p-2 max-w-sm '>
-            <input name='search' type="text" placeholder='Search' className=' rounded-l-md w-full outline-none px-2' onChange={handleSearch} />
+            <input name='search' type="text" placeholder='Search' className=' rounded-l-md w-full outline-none px-2 bg-transparent' onChange={handleSearch} />
             <button className='rounded-r-full flex items-center px-2' >
               <SearchOutlined />
             </button>
